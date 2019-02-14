a2711250f8915f29de4e529f49c6c140509f2509
chichengjerry <chichengjerry@gmail.com>
2019-02-14 17:09:15 +0900

    update

--- 3DGAME/bullet.cpp
+++ 3DGAME/bullet.cpp
@@ -5,20 +5,23 @@
 #include "terrain.h"
 
 #define GM_BULLET_SIZE			10.0f
-#define GM_BULLET_SPEED			10.0f
+#define GM_BULLET_SPEED			300.0f
 #define BULLET_SPRITE			_T("data/TEXTURE/glow01.jpg")
+#define BULLET_WIDE				4.0f
+#define BULLET_PARTICLES		32
 
 BULLET::BULLET(PLAYER * player, D3DXVECTOR3* tar)
 {
 	src = player;
-	bulletType = src->defenseType;
+	bulletType = src->attackType;
 	col = ElementColor[bulletType];
 	bAlive = TRUE;
-	fSpeed = GM_BULLET_SPEED;
+	fSpeed = player->fMoveSpeed + GM_BULLET_SPEED;
 
 	D3DXVECTOR3 mov;
-	if (tar)
+	if (tar) {
 		mov = *tar - player->srt.pos;
+	}
 	else{
 		D3DXMATRIX mtx;
 		D3DXVECTOR4 dir = D3DXVECTOR4(0.0f, 0.0f, 1.0f, 1.0f);
@@ -28,12 +31,14 @@ BULLET::BULLET(PLAYER * player, D3DXVECTOR3* tar)
 	}
 
 	D3DXVec3Normalize(&mov, &mov);
+	mov *= BULLET_WIDE;
 	pos = player->srt.pos;
 
-	bulletParticleEmitter = new EMITTER(BULLET_SPRITE, 32, pos + mov * 32.0f, mov * GM_BULLET_SPEED, col, GM_BULLET_SIZE);
+	bulletParticleEmitter = new EMITTER(BULLET_SPRITE, BULLET_PARTICLES, pos + mov * BULLET_PARTICLES * 2 / BULLET_WIDE, mov * fSpeed, col, GM_BULLET_SIZE);
 	for (int i = 1; i < 32; i++) {
-		bulletParticleEmitter->particles[i].pos = bulletParticleEmitter->particles[i - 1].pos - mov;
-		bulletParticleEmitter->particles[i].scl = (32 - i) / 32.0f;
+		FLOAT scl = (FLOAT)(BULLET_PARTICLES - i) / BULLET_PARTICLES;
+		bulletParticleEmitter->particles[i].scl = scl;
+		bulletParticleEmitter->particles[i].pos = bulletParticleEmitter->particles[i - 1].pos - mov * scl;
 	}
 }
 
